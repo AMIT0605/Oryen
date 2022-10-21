48b94df4da329ac266bd0166e89ad393d6b30929
AMIT0605 <105354634+AMIT0605@users.noreply.github.com>
2022-10-21 17:00:58 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Oryen from './Components/Oryen';
+
+function App() {
+  return (
+   <div>
+    <Oryen/>
+   </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Oryen.js
@@ -0,0 +1,85 @@
+import React, { useState } from "react";
+import "./Oryen.css";
+export default function Oryen() {
+  
+
+  return (
+    <>
+    <div className="card" style={{backgroundColor:"black",width:1400,height:780,position:"absolute"}}/>
+ <div className="card" style={{background:"linear-gradient(#134E5E,#134E5E)",opacity:0.5,width:1000,borderTopLeftRadius:12,borderTopRightRadius:12,borderBottomLeftRadius:12,borderBottomRightRadius:12,left:190,height:550,position:"absolute",top:70}}/>
+
+
+ <img src="https://i.ibb.co/Gpj0vg8/Oryen-Logo-01.png" alt="Oryen-Logo-01" alt="binance-smart-chain-bnb-coin" border="0" style={{width:160,height:160,position:"absolute",top:20,left:180}}/>
+ <div className="box1" style={{opacity:0.5,width:700,left:460,height:80,borderTopLeftRadius:12,borderTopRightRadius:12,borderBottomLeftRadius:12,borderBottomRightRadius:12,position:"absolute",backgroundColor:"transparent",top:180}}/>
+ <p className="txt2" style={{color:"#00b09b",fontSize:12,position:"absolute",top:145,left:470}}>D A S H B O A R D</p>
+ <p className="txt2" style={{color:"#00b09b",fontSize:10,position:"absolute",top:185,left:490}}>Marketcap</p>
+ <p className="txt2" style={{color:"#00b09b",fontSize:10,position:"absolute",top:185,left:700}}>Circulating Supply</p>
+ <p className="txt2" style={{color:"#00b09b",fontSize:10,position:"absolute",top:185,left:900}}>Holders</p>
+ <p className="txt2" style={{color:"#00b09b",fontSize:10,position:"absolute",top:185,left:1070}}>Oryen Price</p>
+ <p style={{color:"white",fontWeight:"bold",fontSize:16,position:"absolute",top:200,left:490}}>$11.97M</p>
+ <p style={{color:"white",fontSize:16,fontWeight:"bold",position:"absolute",top:200,left:700}}>38.35B</p>
+ <p style={{color:"white",fontSize:16,position:"absolute",fontWeight:"bold",top:200,left:900}}>82K</p>
+ <p style={{color:"white",fontSize:16,position:"absolute",top:200,left:1070,fontWeight:"bold"}}>$0.0003</p>
+<div className="box1" style={{opacity:0.5,width:340,left:460,height:80,borderTopLeftRadius:12,borderTopRightRadius:12,borderBottomLeftRadius:12,borderBottomRightRadius:12,position:"absolute",backgroundColor:"transparent",top:280}}/>
+<div className="box1" style={{opacity:0.5,width:340,left:820,height:80,borderTopLeftRadius:12,borderTopRightRadius:12,borderBottomLeftRadius:12,borderBottomRightRadius:12,position:"absolute",backgroundColor:"transparent",top:280}}/>
+<p className="txt2" style={{color:"#00b09b",fontSize:10,position:"absolute",top:285,left:490}}>Liquidity</p>
+<p className="txt2" style={{color:"#00b09b",fontSize:10,position:"absolute",top:285,left:850}}>Treasury</p>
+<p style={{color:"orange",fontSize:10,position:"absolute",top:285,left:740}}>-0.33%</p>
+<p style={{color:"orange",fontSize:10,position:"absolute",top:285,left:1100}}>-0.33%</p>
+<p style={{color:"white",fontWeight:"bold",fontSize:16,position:"absolute",top:300,left:490}}>$1.61M</p>
+<p style={{color:"white",fontWeight:"bold",fontSize:16,position:"absolute",top:300,left:850}}>$8.34M</p>
+<div className="box1" style={{opacity:0.1,width:70,left:492,height:20,position:"absolute",backgroundColor:"white",top:401.5}}/>
+<div className="box2" style={{opacity:0.5,width:700,left:460,height:100,borderWidth:1.5,borderColor:"#00b09b",borderTopLeftRadius:12,borderTopRightRadius:12,borderBottomLeftRadius:12,borderBottomRightRadius:12,position:"absolute",backgroundColor:"transparent",top:390}}/>
+<img src="https://i.ibb.co/Gckr4x3/binance-smart-chain-bnb-coin.png" alt="binance-smart-chain-bnb-coin" border="0" style={{width:135,height:135,position:"absolute",top:375,left:1040}}/>
+<p className="txt2" style={{color:"#00b09b",fontSize:10,position:"absolute",top:395,left:512}}>Fixed APY</p>
+<p style={{color:"white",fontWeight:"bold",fontSize:16,position:"absolute",top:410,left:490}}>102483.58%</p>
+<p style={{color:"#00b09b",fontSize:11,position:"absolute",top:445,left:490}}>A Simple Buy-Hold-Earn System.</p>
+<div className="box1" style={{opacity:0.05,width:340,left:460,height:90,borderTopLeftRadius:12,borderTopRightRadius:12,borderBottomLeftRadius:12,borderBottomRightRadius:12,position:"absolute",backgroundColor:"white",top:510}}/>
+<div className="box1" style={{opacity:0.3,width:340,left:820,height:90,borderTopLeftRadius:12,borderTopRightRadius:12,borderBottomLeftRadius:12,borderBottomRightRadius:12,position:"absolute",backgroundColor:"transparent",top:510}}/>
+<p style={{color:"white",fontSize:16,position:"absolute",fontWeight:"bold",top:510,left:930}}>Connect with us</p>
+<p style={{color:"white",fontSize:16,position:"absolute",fontWeight:"bold",top:510,left:560}}>Still got questions?</p>
+<div className="box1" style={{opacity:0.5,width:130,left:565,height:25,borderTopLeftRadius:5,borderTopRightRadius:5,borderBottomLeftRadius:5,borderBottomRightRadius:5,position:"absolute",backgroundColor:"#00b09b",top:560}}/>
+<p className="txt1" style={{color:"white",fontSize:12,position:"absolute",fontWeight:"bold",top:552,left:594}}>More Details</p>
+<div className="box1" style={{opacity:0.5,width:150,left:1015,height:35,borderTopLeftRadius:5,borderTopRightRadius:5,borderBottomLeftRadius:5,borderBottomRightRadius:5,position:"absolute",backgroundColor:"#00b09b",top:80}}/>
+<p className="txt1" style={{color:"white",fontSize:12,position:"absolute",fontWeight:"bold",top:78,left:1036}}>CONNECT WALLET</p>
+<div className="box1" style={{opacity:0.5,width:150,left:1015,height:35,borderTopLeftRadius:5,borderTopRightRadius:5,borderBottomLeftRadius:5,borderBottomRightRadius:5,position:"absolute",backgroundColor:"#00b09b",top:630}}/>
+<p className="txt1" style={{color:"white",fontSize:12,position:"absolute",fontWeight:"bold",top:628,left:1056}}>BUY  ORYEN</p>
+<p style={{color:"white",fontSize:13,fontWeight:"500",position:"absolute",top:625,left:950}}>$0.003</p>
+<p style={{color:"white",fontSize:13,fontWeight:"500",position:"absolute",top:75,left:950}}>$0.003</p>
+<div className="box3" style={{opacity:0.5,width:1000,left:190,height:1,position:"absolute",backgroundColor:"#00b09b",top:132}}/>
+<div className="box3" style={{opacity:0.5,width:1,left:330,height:550,position:"absolute",backgroundColor:"#00b09b",top:70}}/>
+<p className="txt2" style={{color:"#00b09b",fontSize:12,position:"absolute",top:135,left:230}}>Dashboard</p>
+<p  style={{color:"#00b09b",fontSize:12,position:"absolute",top:135,left:230}}>Dashboard</p>
+<p  style={{color:"#00b09b",fontSize:11,position:"absolute",top:160,left:230}}>Exchange</p>
+<p  style={{color:"#00b09b",fontSize:11,position:"absolute",top:180,left:230}}>-Liquidity</p>
+<p  style={{color:"#00b09b",fontSize:11,position:"absolute",top:200,left:230}}>-Swap</p>
+<p  style={{color:"#00b09b",fontSize:11,position:"absolute",top:230,left:230}}>Calculator</p>
+<p style={{color:"#00b09b",fontSize:11,position:"absolute",top:255,left:230}}>Account</p>
+<p className="txt2" style={{color:"#00b09b",fontSize:12,position:"absolute",top:285,left:210}}>About</p>
+<p style={{color:"#00b09b",fontSize:11,position:"absolute",top:315,left:230}}>Contact</p>
+<p style={{color:"#00b09b",fontSize:11,position:"absolute",top:335,left:230}}>Community</p>
+<p style={{color:"#00b09b",fontSize:11,position:"absolute",top:355,left:230}}>Oryen Token</p>
+<p className="txt2" style={{color:"#00b09b",fontSize:12,position:"absolute",top:385,left:210}}>Help</p>
+<p style={{color:"#00b09b",fontSize:11,position:"absolute",top:415,left:230}}>Documentation</p>
+<p style={{color:"#00b09b",fontSize:11,position:"absolute",top:435,left:230}}>FAQ</p>
+<div className="box1" style={{opacity:0.3,width:120,left:199,height:50,borderTopLeftRadius:5,borderTopRightRadius:5,borderBottomLeftRadius:5,borderBottomRightRadius:5,position:"absolute",backgroundColor:"transparent",top:500}}/>
+<p className="txt2" style={{color:"#00b09b",fontSize:10,fontWeight:"bold",position:"absolute",top:500,left:208}}>SOCIAL CONNECTION</p>
+<img src="https://i.ibb.co/tYCQkS0/twitter-sign.png" alt="twitter-sign" border="0" style={{width:16,height:16,position:"absolute",top:530,left:212}}/>
+<img src="https://i.ibb.co/nBt2C2n/linkedin.png" alt="twitter-sign" border="0" style={{width:16,height:16,position:"absolute",top:530,left:237}}/>
+<img src="https://i.ibb.co/xYMFfkf/facebook.png" alt="twitter-sign" border="0" style={{width:16,height:16,position:"absolute",top:530,left:262}}/>
+<img src="https://i.ibb.co/RSj1qLt/instagram.png" alt="twitter-sign" border="0" style={{width:16,height:16,position:"absolute",top:530,left:287}}/>
+<img src="https://i.ibb.co/tYCQkS0/twitter-sign.png" alt="twitter-sign" border="0" style={{width:26,height:26,position:"absolute",top:560,left:925}}/>
+<img src="https://i.ibb.co/nBt2C2n/linkedin.png" alt="twitter-sign" border="0" style={{width:26,height:26,position:"absolute",top:560,left:960}}/>
+<img src="https://i.ibb.co/xYMFfkf/facebook.png" alt="twitter-sign" border="0" style={{width:26,height:26,position:"absolute",top:560,left:995}}/>
+<img src="https://i.ibb.co/RSj1qLt/instagram.png" alt="twitter-sign" border="0" style={{width:26,height:26,position:"absolute",top:560,left:1030}}/>
+<div className="box4" style={{opacity:0.5,width:20,left:355,height:2,borderTopLeftRadius:5,borderTopRightRadius:5,borderBottomLeftRadius:5,borderBottomRightRadius:5,position:"absolute",backgroundColor:"#00b09b",top:90}}/>
+<div className="box4" style={{opacity:0.5,width:20,left:355,height:2,borderTopLeftRadius:5,borderTopRightRadius:5,borderBottomLeftRadius:5,borderBottomRightRadius:5,position:"absolute",backgroundColor:"#00b09b",top:97}}/>
+<div className="box4" style={{opacity:0.5,width:20,left:355,height:2,borderTopLeftRadius:5,borderTopRightRadius:5,borderBottomLeftRadius:5,borderBottomRightRadius:5,position:"absolute",backgroundColor:"#00b09b",top:104}}/>
+<img src="https://i.ibb.co/4m3g46M/Oryen-Logo-011.png" alt="twitter-sign" border="0" style={{width:76,height:106,position:"absolute",top:500,left:465}}/>
+
+
+
+
+    </>
+  );
+}
